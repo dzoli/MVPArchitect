db54b22b6d7fad9dbbc4f3a05ca49356bd506cc6
dzoli <bje.no9@gmail.com>
2018-06-07 02:16:38 +0200

    refactoring

--- app/src/main/java/dmi/pmf/novica/mvparchitect/ui/games/GamePresenter.java
+++ app/src/main/java/dmi/pmf/novica/mvparchitect/ui/games/GamePresenter.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import dmi.pmf.novica.mvparchitect.twitch.model.Top;
 
-public class GamePresenter implements GamesActivityMVP.Presenter {
+public class GamePresenter implements GamesActivityMVP.Presenter, GamesCallback{
 
     GamesActivityMVP.View view;
     GamesActivityMVP.Model model;
@@ -18,23 +18,22 @@ public class GamePresenter implements GamesActivityMVP.Presenter {
     @Override
     public void getAllData() {
         if (view != null) {
-            model.getGamesFromAPI(new GamesCallback() {
-                @Override
-                public void onSuccess(@NonNull List<Top> gamesList) {
-                    view.fillAdapter(gamesList);
-                }
-
-                @Override
-                public void onError(@NonNull Throwable throwable) {
-                    view.showLoadErrorMessage();
-                }
-            });
+            model.getGamesFromAPI(this);
         }
-
     }
 
     @Override
     public void setView(GamesActivityMVP.View view) {
         this.view = view;
+    }
+
+    @Override
+    public void onSuccess(@NonNull List<Top> gamesList) {
+        view.fillAdapter(gamesList);
+    }
+
+    @Override
+    public void onError(@NonNull Throwable throwable) {
+        view.showLoadErrorMessage();
     }
 }
